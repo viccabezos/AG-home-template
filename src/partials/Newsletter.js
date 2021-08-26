a51da14a8b86ec97bcd4dcd895c04f80960f3f2c
viccabezos <cabezosvic@gmail.com>
2021-08-26 11:57:59 +0200

    changes footer and color style

--- src/partials/Newsletter.js
+++ src/partials/Newsletter.js
@@ -7,36 +7,9 @@ function Newsletter() {
         <div className="pb-12 md:pb-20">
           {/* CTA box */}
           <div
-            className="relative bg-gray-900 rounded py-10 px-8 md:py-16 md:px-12 shadow-2xl overflow-hidden"
+            className="relative flex justify-center bg-blue-300 rounded py-10 px-8 md:py-16 md:px-12 shadow-2xl overflow-hidden"
             data-aos="zoom-y-out"
           >
-            {/* Background illustration */}
-            <div
-              className="absolute right-0 bottom-0 pointer-events-none hidden lg:block"
-              aria-hidden="true"
-            >
-              <svg width="428" height="328" xmlns="http://www.w3.org/2000/svg">
-                <defs>
-                  <radialGradient
-                    cx="35.542%"
-                    cy="34.553%"
-                    fx="35.542%"
-                    fy="34.553%"
-                    r="96.031%"
-                    id="ni-a"
-                  >
-                    <stop stopColor="#DFDFDF" offset="0%" />
-                    <stop stopColor="#4C4C4C" offset="44.317%" />
-                    <stop stopColor="#333" offset="100%" />
-                  </radialGradient>
-                </defs>
-                <g fill="none" fillRule="evenodd">
-                  <g fill="#FFF"></g>
-                  <circle fill="url(#ni-a)" cx="276" cy="237" r="200" />
-                </g>
-              </svg>
-            </div>
-
             <div className="relative flex flex-col lg:flex-row justify-between items-center">
               {/* CTA content */}
               <div className="text-center lg:text-left lg:max-w-xl">
@@ -51,13 +24,13 @@ function Newsletter() {
                   <div className="flex flex-col space-y-2 justify-center max-w-xs mx-auto sm:max-w-md lg:mx-0">
                     <input
                       type="email"
-                      className="w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500"
+                      className="w-full appearance-none bg-blue-300 border border-white focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500"
                       placeholder="Your email…"
                       aria-label="Your email…"
                     />
                     <input
                       type="message"
-                      className="w-full appearance-none bg-gray-800 border border-gray-700 focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500"
+                      className="w-full appearance-none bg-blue-300 border border-white focus:border-gray-600 rounded-sm px-4 py-3 mb-2 sm:mb-0 sm:mr-2 text-white placeholder-gray-500"
                       placeholder="message"
                       aria-label="Your email…"
                     />
